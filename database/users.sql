a8c3dad6ceb33ec3e541ec4a27f3d4a60eac7fa9
Alessandro Paolantonio <alexpaolantonio@gmail.com>
2024-05-10 13:17:35 +0200

    modifica database utenti

---
+++ database/users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE utenti (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cognome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    indirizzo VARCHAR(100) NOT NULL,
+    n_civico VARCHAR(5) NOT NULL,
+    citta VARCHAR(100) NOT NULL
+);
