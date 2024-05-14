ea91985c0651dc9118e0c11c8e7090584a60b0d7
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2024-05-14 15:15:34 +0200

    SHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET

--- account.php
+++ account.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="styleshett" href="styles/account.css">
+    <link rel="stylesheet" href="styles/account.css">
         
 
 
