2f28253797b39a5b825673548df6c51f92aaab82
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2024-05-14 10:37:58 +0200

    Fatto gazada

--- login/layouts/footer.php
+++ login/layouts/footer.php
@@ -1,2 +1,63 @@
-</body>
-</html>
+    <footer class="footer footer-bg">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md-8">
+              <div class="row">
+                <div class="col-md">
+                  <div class="mb-4">
+                    <img src="../img/logo_footer.png" style="width: 75%;">
+                  </div>
+                </div>
+                <div class="col-md">
+                  <div class="mb-4">
+                    <h2 class="footer-heading">Navigazione</h2>
+                    <ul class="list-unstyled">
+                      <li><a href="../index.html" class="py-2 d-block">Home</a></li>
+                      <li><a href="caffe.html" class="py-2 d-block">Caffe</a></li>
+                      <li><a href="custom.html" class="py-2 d-block">Create</a></li>
+                      <li><a href="contatti.html" class="py-2 d-block">Contatti</a></li>
+                      <li><a href="account.html" class="py-2 d-block">Account</a></li>
+                      <li><a href="carrello.html" class="py-2 d-block">Carrello</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-md">
+                  <div class="mb-4">
+                    <h2 class="footer-heading">Contatti</h2>
+                    <ul class="list-unstyled">
+                      <li><a href="tel:0123456789" class="py-2 d-block">0123 456789</a></li>
+                      <li><a href="mailto:info@e_spresso.it" class="py-2 d-block">info@e_spresso.it</a></li>
+                      <li><a target="_blank" href="#" class="py-2 d-block">Via Roma 17 00159, Roma</a></li>
+                      <li><a target="_blank" href="#" class="py-2 d-block">Lascia una recensione!</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="mb-4">
+                <ul class="footer-social list-unstyled ">
+                  <li><a target="_blank" href="https://www.facebook.com/"><span class="bi bi-facebook"></span></a></li>
+                  <li><a target="_blank" href="https://www.instagram.com/"><span class="bi bi-instagram"></span></a></li>
+                  <li><a target="_blank" href="https://wa.me/"><span class="bi bi-whatsapp"></span></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col- text-left">
+              <p><a target="_blank" href="#">Informativa sulla privacy</a>  /  <a target="_blank" href="#">Cookie policy</a></p>
+              <p>&copy; Exclusivity 2023. All Rights Reserved. Made by the E-Spresso Team.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      <script src="../scripts/script.js"></script>
+
+    </body>
+</html
