36ea316e40252040a4702269576710af045719f3
Alessandro Paolantonio <alexpaolantonio@gmail.com>
2024-05-15 10:47:37 +0200

    aggiustata connessione al database utenti

--- login/utility/config.php
+++ login/utility/config.php
@@ -3,14 +3,10 @@
 /* LATO SERVER */
 
 //PDO Connessione al db
-$servername = "localhost";
-$username = "postgres";
-$passworddb = "admin";
-$dbname = "users";
 
 try {
-    $dbh = new PDO("pgsql:$servername;dbname=$dbname", $username, $passworddb);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  /* connette gli attributi del server */
+    $pdo = new PDO('pgsql:host=localhost;dbname=users', 'postgres', 'admin');
+    $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connessione riuscita";
 } catch (PDOException $e) {
     print "Errore di connessione: " . $e->getMessage() . "<br/>";
