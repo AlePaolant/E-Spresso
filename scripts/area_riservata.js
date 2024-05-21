126caf788897af1eec69970f97b6503418f5df02
Alessandro Nardi <nardi.1853941@studenti.uniroma1.it>
2024-05-21 18:50:51 +0200

    creata la connessione tra il database e l'area riservata, cambiati un pò la pagina dell'area riservata e nel suo css

--- scripts/area_riservata.js
+++ scripts/area_riservata.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var clickableArea = document.getElementById("clickableArea");
+    var table = document.getElementById("clickableTable");
+    var cells = table.getElementsByTagName("td");
 
-    clickableArea.addEventListener("click", function() {
-        this.classList.toggle("clicked");
-    });
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", function() {
+            for (var j = 0; j < cells.length; j++) {
+                cells[j].classList.remove("clicked");  // Rimuove la classe 'clicked' da tutte le celle
+            }
+            this.classList.toggle("clicked");// Aggiunge la classe 'clicked' solo alla cella cliccata
+        });
+    }
 });
